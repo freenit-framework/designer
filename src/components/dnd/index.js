abec67d38875661df05f881429b78625ed7ad6b5
Goran Mekić <meka@tilda.center>
2020-01-15 22:03:33 +0100

    Be more specific checking drop target

--- src/components/dnd/index.js
+++ src/components/dnd/index.js
@@ -8,23 +8,22 @@ import types from 'types'
 const DnD = (props) => {
   const { identity } = props
   const { design } = props.store
-  const [{ canDrop, isOver, isOverCurrent }, drop] = useDrop({
+  const [{ canDrop, isOver }, drop] = useDrop({
     accept: types.COMPONENT,
     drop: (item, monitor) => {
-      if (monitor.isOver({ shallow:true })) {
+      if (monitor.isOver({ shallow:true }) && monitor.canDrop()) {
         design.add(item, identity)
       }
     },
     collect: monitor => ({
-      isOver: monitor.isOver(),
-      isOverCurrent: monitor.isOver({ shallow: true }),
+      isOver: monitor.isOver({ shallow: true }),
       canDrop: monitor.canDrop(),
     }),
   })
   let style
-  if (canDrop && isOver && isOverCurrent) {
+  if (canDrop && isOver) {
     style = { border: '1px dashed green' }
-  } else if (props.store.design.selected.identity === identity){
+  } else if (props.store.design.selected.identity === identity) {
     style = { border: '1px dashed red' }
   } else {
     style = null
