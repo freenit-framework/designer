7c5a9008a513becec1d3071a1dc75922df0071c2
Goran Mekić <meka@tilda.center>
2020-01-20 10:34:34 +0100

    Add tabs

--- src/components/display/index.js
+++ src/components/display/index.js
@@ -1,14 +1,37 @@
 import React from 'react'
 import { withStore } from 'freenit'
 import DnD from 'components/dnd'
+import {
+  Tab,
+  Tabs,
+} from '@material-ui/core'
 import styles from './styles'
 
 
 class Display extends React.Component {
+  state = {
+    value: 0,
+  }
+
+  handleChange = (event, value) => {
+    this.setState({ value })
+  }
+
   render() {
     const { tree } = this.props.store.design
     return (
       <div style={styles.root}>
+        <Tabs
+          value={this.state.value}
+          indicatorColor="primary"
+          textColor="primary"
+          onChange={this.handleChange}
+          variant="fullWidth"
+        >
+          <Tab label="Design" />
+          <Tab label="Save" />
+          <Tab label="Export" />
+        </Tabs>
         <DnD data={tree} />
       </div>
     )
