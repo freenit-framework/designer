973464e8897e387ff779db892c0ea92a037f7aaf
Goran Mekić <meka@tilda.center>
2020-01-22 11:18:35 +0100

    Scrollbar on output of export and save

--- src/components/export/styles.js
+++ src/components/export/styles.js
@@ -1,5 +1,7 @@
 export default {
   root: {
     padding: 10,
+    overflowY: 'auto',
+    height: 'calc(100vh - 48px - 20px - 5px)'
   },
 }
--- src/components/save/styles.js
+++ src/components/save/styles.js
@@ -1,6 +1,8 @@
 export default {
   root: {
     padding: 10,
+    overflowY: 'auto',
+    height: 'calc(100vh - 48px - 20px - 5px)'
   },
 
   input: {
