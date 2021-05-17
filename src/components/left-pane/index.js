f733b7f7271e183c0afeb37c2b61f9d2570317cf
Goran Mekić <meka@tilda.center>
2021-05-18 01:10:12 +0200

    Filter components

--- src/components/left-pane/index.js
+++ src/components/left-pane/index.js
@@ -9,10 +9,51 @@ import styles from './styles'
 class LeftPane extends React.Component {
   state = {
     open: true,
+    search: '',
+    caseSensitive: true,
+    func: false,
   }
 
   toggleOpen = () => {
     this.setState({ open: !this.state.open })
+  }
+
+  toggleCase = () => {
+    this.setState({ caseSensitive: !this.state.caseSensitive })
+  }
+
+  toggleFunc = () => {
+    this.setState({ func: !this.state.func })
+  }
+
+  changeSearch = (event) => {
+    this.setState({ search: event.target.value })
+  }
+
+  filterComponents = () => {
+    const result = {
+      mui: {},
+      html: {},
+    }
+    const rawMuiNames = Object.keys(components.mui)
+    const muiNames = this.state.caseSensitive
+      ? rawMuiNames.filter((name) => name.includes(this.state.search))
+      : rawMuiNames.filter((name) =>
+          name.toLowerCase().includes(this.state.search)
+        )
+    muiNames.forEach((name) => {
+      result.mui[name] = components.mui[name]
+    })
+    const rawHtmlNames = Object.keys(components.html)
+    const htmlNames = this.state.caseSensitive
+      ? rawHtmlNames.filter((name) => name.includes(this.state.search))
+      : rawHtmlNames.filter((name) =>
+          name.toLowerCase().includes(this.state.search)
+        )
+    htmlNames.forEach((name) => {
+      result.mui[name] = components.html[name]
+    })
+    return result
   }
 
   render() {
@@ -38,15 +79,16 @@ class LeftPane extends React.Component {
         </IconButton>
       </div>
     )
+    const filteredComponents = this.filterComponents(components)
     const muiComponents = this.state.open
-      ? Object.keys(components.mui).map((name) => (
+      ? Object.keys(filteredComponents.mui).map((name) => (
           <div key={`mui-${name}`} style={styles.component}>
             {name}
           </div>
         ))
       : null
     const htmlComponents = this.state.open
-      ? Object.keys(components.html).map((name) => (
+      ? Object.keys(filteredComponents.html).map((name) => (
           <div key={`html-${name}`} style={styles.component}>
             {name}
           </div>
@@ -61,13 +103,23 @@ class LeftPane extends React.Component {
         }
     const search = this.state.open ? (
       <div style={styles.search}>
-        <TextField label="Search" style={styles.search.text} />
-        <Paper style={styles.search.button}>A</Paper>
+        <TextField
+          label="Search"
+          style={styles.search.text}
+          onChange={this.changeSearch}
+        />
+        <Paper style={styles.search.button} onClick={this.toggleCase}>
+          {this.state.caseSensitive ? 'A' : 'a'}
+        </Paper>
       </div>
     ) : null
-    const control = this.state.open ? (
+    const fileControl = this.state.open ? (
       <Paper style={styles.file}>
-        <Switch />
+        <Switch
+          checked={!this.state.func}
+          color="primary"
+          onChange={this.toggleFunc}
+        />
         Class Component
         <div style={styles.file.controls}>
           <Button variant="outlined" color="primary">
@@ -88,7 +140,7 @@ class LeftPane extends React.Component {
           {muiComponents}
           {htmlComponents}
         </div>
-        {control}
+        {fileControl}
       </div>
     )
   }
