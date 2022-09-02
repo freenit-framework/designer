61748ead7f288e2222b65d21197d037a9050cec0
Goran Mekić <meka@tilda.center>
2022-09-02 23:26:21 +0200

    Update Svelte

--- bin/common.sh
+++ bin/common.sh
@@ -4,28 +4,23 @@
 export BIN_DIR=`dirname $0`
 export PROJECT_ROOT="${BIN_DIR}/.."
 export OFFLINE=${OFFLINE:=no}
-NPM=`which npm 2>/dev/null`
 YARN=`which yarn 2>/dev/null`
 
 
-if [ ! -z "${NPM}" ]; then
-  export PACKAGE_MANAGER="${NPM} run"
-  export PACKAGE_MANAGER_INSTALL="${NPM}"
-else
+if [ ! -z "${YARN}" ]; then
   export PACKAGE_MANAGER="${YARN}"
-  export PACKAGE_MANAGER_INSTALL="${YARN}"
 fi
 
 
 setup() {
   if [ -z "${PACKAGE_MANAGER}" ]; then
-    echo "Install npm or yarn" >&2
+    echo "Install yarn" >&2
     exit 1
   fi
 
   cd ${PROJECT_ROOT}
   update=${1}
   if [ "${OFFLINE}" != "yes" -a "${update}" != "no" ]; then
-    ${PACKAGE_MANAGER_INSTALL} install
+    ${PACKAGE_MANAGER} install
   fi
 }
