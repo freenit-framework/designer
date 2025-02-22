b052fd5fd9fa290b2ace9edb58bf8eec05f704cb
Goran Mekić <meka@tilda.center>
2025-02-22 18:35:01 +0100

    Left pane

---
+++ src/lib/LeftPane.svelte
@@ -0,0 +1,312 @@
+<script lang="ts">
+  const data = {
+    dJEPTZXw: '}',
+    fPmBcrDx: 'css: {',
+    fYEQOCGM: '+',
+    ewXLBPxm: 'height: 100dvh',
+    LBNeQsoT: '-',
+    ZQOMzkHs: '}',
+    PDIBWGoa: 'text:',
+    yqlmVfcU: 'Save',
+    JKUPZXSw: 'Load',
+    LBCekUGr: 'Export',
+  }
+</script>
+
+<div class="root">
+  <div class="qIPjcrNt">
+    <button class="button outline" disabled> Components </button>
+    <button class="button outline"> Icons </button>
+    <button disabled class="button outline"> Props </button>
+    <button class="button outline"> Theme </button>
+    <svg class="WLSdUpuk">
+      <path d="M20,9V15H12V19.84L4.16,12L12,4.16V9H20Z" />
+    </svg>
+  </div>
+  <div class="BcKPWMgC">
+    <input class="DEuCAyzJ" value="div" />
+    <div class="pRKEiDhA">
+      <svg class="hSAcIvKS">
+        <path
+          d="M12.5,8C9.85,8 7.45,9 5.6,10.6L2,7V16H11L7.38,12.38C8.77,11.22 10.54,10.5 12.5,10.5C16.04,10.5 19.05,12.81 20.1,16L22.47,15.22C21.08,11.03 17.15,8 12.5,8Z"
+        />
+      </svg>
+      <svg class="KZSropox">
+        <path
+          d="M18.4,10.6C16.55,9 14.15,8 11.5,8C6.85,8 2.92,11.03 1.54,15.22L3.9,16C4.95,12.81 7.95,10.5 11.5,10.5C13.45,10.5 15.23,11.22 16.62,12.38L13,16H22V7L18.4,10.6Z"
+        />
+      </svg>
+      <svg class="OdZLNMLh">
+        <path
+          d="M19.31 18.9C19.75 18.21 20 17.38 20 16.5C20 14 18 12 15.5 12S11 14 11 16.5 13 21 15.5 21C16.37 21 17.19 20.75 17.88 20.32L21 23.39L22.39 22L19.31 18.9M15.5 19C14.12 19 13 17.88 13 16.5S14.12 14 15.5 14 18 15.12 18 16.5 16.88 19 15.5 19M21 9H19V7H21V9M21 5H19V3H20C20.55 3 21 3.45 21 4V5M19 11.03V11H21V13H20.97C20.46 12.21 19.79 11.54 19 11.03M17 5H15V3H17V5M13 5H11V3H13V5M3 7H5V9H3V7M7 19H9V21H7V19M3 11H5V13H3V11M4 3H5V5H3V4C3 3.45 3.45 3 4 3M9 5H7V3H9V5M3 19H5V21H4C3.45 21 3 20.55 3 20V19M3 15H5V17H3V15Z"
+        />
+      </svg>
+    </div>
+  </div>
+  <div class="GqdjBrtE">
+    <div class="WQWcYDkX">
+      <div class="NySYmwaO">div</div>
+    </div>
+    <div class="RBPAQFwm">
+      <div class="AFetiBdc">
+        <div class="NStkhGwA">
+          <div class="uHCxfJqK">
+            <div class="aKbCqmqL">
+              div
+            </div>
+            <div class="IUxlpXlW">
+              IUxlpXlW
+            </div>
+          </div>
+          <div class="uHiQAzOI">
+            <svg class="fXdsEZkN">
+              <path
+                d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
+              />
+            </svg>
+          </div>
+          <div class="ufBQjmyA">
+            <svg class="RRiJlHbv">
+              <path d="M8 15H16L12 8" />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div class="mXObqkgw">
+        <div class="JNwQTEEd">
+          <span class="UOGIzAtn">
+            props: &#123; +
+          </span>
+        </div>
+        <div class="xYIaEXzN">
+          <span class="XukmDQIf">
+            class: root -
+          </span>
+        </div>
+        <div class="dJEPTZXw">
+          &#125;
+        </div>
+        <div class="fPmBcrDx">
+          <span class="fYEQOCGM">
+            {data.fYEQOCGM}
+          </span>
+          {data.fPmBcrDx}
+        </div>
+        <div class="ewXLBPxm">
+          <span class="LBNeQsoT">
+            {data.LBNeQsoT}
+          </span>
+          {data.ewXLBPxm}
+        </div>
+        <div class="ZQOMzkHs">
+          {data.ZQOMzkHs}
+        </div>
+        <div class="PDIBWGoa">
+          {data.PDIBWGoa}
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="WfzysOUx">
+    <div class="CztkGxAa">
+      <button class="button outline">
+        {data.yqlmVfcU}
+      </button>
+      <button class="button outline primary">
+        {data.JKUPZXSw}
+      </button>
+      <button class="button outline">
+        {data.LBCekUGr}
+      </button>
+    </div>
+    <div class="nQCFizSh">
+      <svg class="PlfaAmFZ">
+        <path
+          d="M17,19H7V5H17M17,1H7C5.89,1 5,1.89 5,3V21A2,2 0 0,0 7,23H17A2,2 0 0,0 19,21V3C19,1.89 18.1,1 17,1Z"
+        />
+      </svg>
+      <svg class="OZQBPTfe">
+        <path
+          d="M19,18H5V6H19M21,4H3C1.89,4 1,4.89 1,6V18A2,2 0 0,0 3,20H21A2,2 0 0,0 23,18V6C23,4.89 22.1,4 21,4Z"
+        />
+      </svg>
+      <svg class="BGcOJqLi">
+        <path
+          d="M4,6H20V16H4M20,18A2,2 0 0,0 22,16V6C22,4.89 21.1,4 20,4H4C2.89,4 2,4.89 2,6V16A2,2 0 0,0 4,18H0V20H24V18H20Z"
+        />
+      </svg>
+    </div>
+  </div>
+</div>
+
+<style>
+  .root {
+    height: 100%;
+    width: 600px;
+    background-color: #fafaff;
+    display: flex;
+    align-items: stretch;
+    justify-content: center;
+    flex-direction: column;
+    border-right: dotted 1px gray;
+  }
+
+  .qIPjcrNt {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    background-color: white;
+    padding: 5px;
+  }
+
+  .WLSdUpuk {
+    width: 26px;
+    height: 26px;
+    fill: #666;
+  }
+
+  .BcKPWMgC {
+    padding: 5px;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+  }
+
+  .DEuCAyzJ {
+    width: 50%;
+  }
+
+  .pRKEiDhA {
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  .hSAcIvKS {
+    width: 26px;
+    height: 26px;
+  }
+
+  .KZSropox {
+    width: 26px;
+    height: 26px;
+  }
+
+  .OdZLNMLh {
+    width: 26px;
+    height: 26px;
+  }
+
+  .GqdjBrtE {
+    flex: 1;
+    display: flex;
+    align-items: stretch;
+    justify-content: center;
+  }
+
+  .WQWcYDkX {
+    width: 50%;
+    padding: 5px;
+  }
+
+  .NySYmwaO {
+    padding: 5px;
+    border-radius: 5px;
+    background-color: rgba(128, 128, 128, 0.1);
+    margin-bottom: 5px;
+  }
+
+  .RBPAQFwm {
+    width: 50%;
+    padding: 5px;
+  }
+
+  .AFetiBdc {
+    height: 50%;
+  }
+
+  .NStkhGwA {
+    border-radius: 5px;
+    background-color: rgba(128, 128, 128, 0.1);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-left: 5px;
+  }
+
+  .uHCxfJqK {
+    flex: 1;
+    padding: 5px;
+  }
+
+  .IUxlpXlW {
+    color: #888;
+  }
+
+  .fXdsEZkN {
+    width: 26px;
+    height: 26px;
+    fill: #666;
+  }
+
+  .RRiJlHbv {
+    width: 26px;
+    height: 26px;
+    fill: #666;
+  }
+
+  .mXObqkgw {
+    margin-top: 5px;
+    border-top: dotted 1px gray;
+    padding: 5px;
+    height: 50%;
+  }
+
+  .xYIaEXzN {
+    margin-left: 10px;
+  }
+
+  .ewXLBPxm {
+    margin-left: 10px;
+  }
+
+  .WfzysOUx {
+    background-color: white;
+    padding: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .CztkGxAa {
+    width: 50%;
+    justify-content: space-around;
+    align-items: center;
+    display: flex;
+  }
+
+  .nQCFizSh {
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  .PlfaAmFZ {
+    width: 26px;
+    height: 26px;
+  }
+
+  .OZQBPTfe {
+    width: 26px;
+    height: 26px;
+  }
+
+  .BGcOJqLi {
+    width: 26px;
+    height: 26px;
+  }
+</style>
--- src/routes/design/+page.svelte
+++ src/routes/design/+page.svelte
@@ -1,5 +1,24 @@
 <script lang="ts">
-  import store from '$lib/store'
+  import LeftPane from '$lib/LeftPane.svelte'
 </script>
 
-<div>design</div>
+<div class="root">
+  <LeftPane />
+  <div class="render">
+  </div>
+</div>
+
+<style>
+  .root {
+    height: 100dvh;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .render {
+    height: 100%;
+    flex: 1;
+  }
+</style>
