60c6e0c6b8181d14aeedb87041302804eace4952
Goran Mekić <meka@tilda.center>
2025-02-23 16:04:14 +0100

    Element as separate component

---
+++ src/lib/LeftPane/Element.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+  import { mdiClose, mdiMenuUp } from '@mdi/js'
+  import Element from './Element.svelte'
+  import store from '$lib/store'
+  import type { Component } from '$lib/types'
+
+  let { element, parent } = $props()
+
+  const allowDrop = (event: Event) => {
+    event.preventDefault()
+  }
+
+  const drop = (component: Component) => (event: DragEvent) => {
+    event.preventDefault()
+    event.stopPropagation()
+    const json = event.dataTransfer ? event.dataTransfer.getData('component') : '{}'
+    const data: Component = JSON.parse(json)
+    component.children.push(data)
+    console.log(component)
+  }
+
+  const toggleOpen = (component: Component) => () => {
+    component.open = !component.open
+  }
+
+  const close = (parent: Component | null, component: Component) => () => {
+    if (parent) {
+      parent.children = parent.children.filter((element) => element != component)
+    } else {
+      store.design.design = store.design.design.filter((element: Component) => element != component)
+    }
+  }
+</script>
+
+<div class="root" ondragover={allowDrop} ondrop={drop(element)} role="none">
+  <div class="element">
+    <div class="data">
+      <div>{element.name}</div>
+      <div class="id">{element.id}</div>
+    </div>
+    <div>
+      <svg class="icon" onclick={close(parent, element)} role="none">
+        <path d={mdiClose} />
+      </svg>
+    </div>
+    <div>
+      <svg class="icon" onclick={toggleOpen(element)} role="none">
+        <path d={mdiMenuUp} />
+      </svg>
+    </div>
+  </div>
+  {#if element.open}
+    {#each element.children as child}
+      <Element element={child} parent={element} />
+    {/each}
+  {/if}
+</div>
+
+<style>
+  .root {
+    border-radius: 5px;
+    border: 1px solid #ddd;
+    background-color: rgba(128, 128, 128, 0.1);
+    padding-left: 5px;
+    margin-top: 2px;
+  }
+
+  .element {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .data {
+    flex: 1;
+    padding: 5px;
+  }
+
+  .id {
+    color: #888;
+  }
+
+  .icon {
+    width: 26px;
+    height: 26px;
+    fill: #666;
+  }
+</style>
--- src/lib/LeftPane/Tree.svelte
+++ src/lib/LeftPane/Tree.svelte
@@ -1,40 +1,11 @@
 <script lang="ts">
-  import { mdiClose, mdiMenuUp } from '@mdi/js'
   import store from '$lib/store'
-  import type { Component } from '$lib/types'
-
-  const allowDrop = (event: Event) => {
-    event.preventDefault()
-  }
-
-  const drop = (component: Component) => (event: DragEvent) => {
-    event.preventDefault()
-    event.stopPropagation()
-    const json = event.dataTransfer ? event.dataTransfer.getData('component') : '{}'
-    const data: Component = JSON.parse(json)
-    component.children.push(data)
-    console.log(component)
-  }
+  import Element from './Element.svelte'
 </script>
 
 <div class="tree">
   {#each store.design.design as element}
-    <div class="element" ondragover={allowDrop} ondrop={drop(element)} role="none">
-      <div class="data">
-        <div>{element.name}</div>
-        <div class="id">{element.id}</div>
-      </div>
-      <div>
-        <svg class="icon">
-          <path d={mdiClose} />
-        </svg>
-      </div>
-      <div>
-        <svg class="icon">
-          <path d={mdiMenuUp} />
-        </svg>
-      </div>
-    </div>
+    <Element {element} />
   {/each}
 </div>
 
@@ -42,31 +13,5 @@
   .tree {
     height: 50%;
     overflow: auto;
-  }
-
-  .element {
-    border-radius: 5px;
-    border: 1px solid #ddd;
-    background-color: rgba(128, 128, 128, 0.1);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding-left: 5px;
-    margin-top: 2px;
-  }
-
-  .data {
-    flex: 1;
-    padding: 5px;
-  }
-
-  .id {
-    color: #888;
-  }
-
-  .icon {
-    width: 26px;
-    height: 26px;
-    fill: #666;
   }
 </style>
