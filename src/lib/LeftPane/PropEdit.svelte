4a3580bcdb61dc34b97a108f4cd93b21c8aa62e1
Goran Mekić <meka@tilda.center>
2025-04-19 01:02:36 +0200

    Drop down with all CSS props

--- src/lib/LeftPane/PropEdit.svelte
+++ src/lib/LeftPane/PropEdit.svelte
@@ -4,9 +4,16 @@
   import { colord } from 'colord'
   import mytype from '$lib/mytype'
   import type { Colord } from 'colord'
+  import cssProps from '$lib/all-css-props'
 
   const hex = '#000000'
-  let { notype = $bindable(false), value = $bindable(), name = $bindable(''), noname = $bindable(false) } = $props()
+  let {
+    notype = $bindable(false),
+    value = $bindable(),
+    name = $bindable(''),
+    noname = $bindable(false),
+    css = $bindable(false),
+  } = $props()
 
   const changeType = (event: Event) => {
     const mytype = (event.target as HTMLInputElement).value
@@ -23,16 +30,23 @@
 </script>
 
 {#if !notype}
-<select value={mytype(value)} onchange={changeType}>
-  <option value="string">string</option>
-  <option value="number">number</option>
-  <option value="bool">bool</option>
-  <option value="color">color</option>
-</select>
+  <select value={mytype(value)} onchange={changeType}>
+    <option value="string">string</option>
+    <option value="number">number</option>
+    <option value="bool">bool</option>
+    <option value="color">color</option>
+  </select>
 {/if}
 
 {#if !noname}
-  <Input label="name" type="text" name="name" bind:value={name} autofocus />
+  <Input label="name" type="text" name="name" bind:value={name} autofocus list="css-list" />
+  {#if css}
+    <datalist id="css-list">
+      {#each cssProps as prop}
+        <option value={prop}></option>
+      {/each}
+    </datalist>
+  {/if}
 {/if}
 {#if mytype(value) === 'string'}
   <Input label="value" type="text" name="value" bind:value />
