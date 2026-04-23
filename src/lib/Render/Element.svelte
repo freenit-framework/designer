e7dba1a95f6a75653f5b828b6b21297e768e7ed3
Goran Mekić <meka@tilda.center>
2026-04-24 00:09:16 +0200

    Click in render also selects

--- src/lib/Render/Element.svelte
+++ src/lib/Render/Element.svelte
@@ -4,6 +4,13 @@
   import Element from './Element.svelte'
 
   const { component } = $props()
+
+  const select = (event: MouseEvent) => {
+    event.preventDefault()
+    event.stopPropagation()
+    store.design.selected = component
+  }
+
   let css = $derived.by(() => {
     if (store.design.selected?.id === component.id) {
       return style({ ...component.css, border: '1px dotted gray' })
@@ -12,7 +19,7 @@
   })
 </script>
 
-<component.component {...component.props} style={css} data-testid={component.id}>
+<component.component {...component.props} style={css} data-testid={component.id} onclick={select}>
   {#each component.children as child}
     <Element component={child} />
   {/each}
