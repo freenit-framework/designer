c9e1116c266f1f33cc2d2cc3566d18fb2e4f5ad2
Goran Mekić <meka@tilda.center>
2026-04-24 02:05:26 +0200

    Handle elements without props and children

--- src/lib/export.js
+++ src/lib/export.js
@@ -8,6 +8,46 @@ function renderValue(value) {
 }
 
 const IDENTIFIER_RE = /^[A-Za-z_$][A-Za-z0-9_$]*$/
+const NO_CHILDREN_ELEMENTS = new Set([
+  'img',
+  'hr',
+  'br',
+  'wbr',
+  'area',
+  'base',
+  'col',
+  'embed',
+  'input',
+  'link',
+  'param',
+  'source',
+  'track',
+  'textarea',
+])
+const NO_PROPS_ELEMENTS = new Set(['textarea', 'br'])
+const VOID_ELEMENTS = new Set(['br'])
+
+function escapeSingleQuoted(value) {
+  return String(value)
+    .replace(/\\/g, '\\\\')
+    .replace(/'/g, "\\'")
+    .replace(/\r/g, '\\r')
+    .replace(/\n/g, '\\n')
+    .replace(/\u2028/g, '\\u2028')
+    .replace(/\u2029/g, '\\u2029')
+}
+
+function canHaveChildren(tagName) {
+  return !NO_CHILDREN_ELEMENTS.has(tagName)
+}
+
+function canHaveProps(tagName) {
+  return !NO_PROPS_ELEMENTS.has(tagName)
+}
+
+function isVoid(tagName) {
+  return VOID_ELEMENTS.has(tagName)
+}
 
 function getIconName(component) {
   const title = component?.title
@@ -25,11 +65,14 @@ function getIconName(component) {
 
 function calculateDataNode(component) {
   let ret = ''
-  if (component.text) {
-    ret += `    '${component.id}': '${component.text}',\n`
+  const tagName = component.name.toLowerCase()
+  if (canHaveChildren(tagName) && component.text) {
+    ret += `    '${escapeSingleQuoted(component.id)}': '${escapeSingleQuoted(component.text)}',\n`
   }
-  for (const child of component.children) {
-    ret += calculateDataNode(child)
+  if (canHaveChildren(tagName)) {
+    for (const child of component.children) {
+      ret += calculateDataNode(child)
+    }
   }
   return ret
 }
@@ -67,33 +110,46 @@ export function calculateCss(design) {
 }
 
 function calculateComponentNode(component, level = 0) {
+  const tagName = component.name.toLowerCase()
+  const allowChildren = canHaveChildren(tagName)
+  const allowProps = canHaveProps(tagName)
+  const elementIsVoid = isVoid(tagName)
+  const selfClosing = elementIsVoid || (!allowChildren && allowProps)
   let ret = ' '.repeat(level)
-  ret += `<${component.name.toLowerCase()}`
-  if (Object.keys(component.css).length > 0) {
+  ret += `<${tagName}`
+  if (allowProps && Object.keys(component.css).length > 0) {
     ret += ` class="${component.id}"`
   }
-  for (const prop in component.props) {
-    if (component.name === 'Path' && prop === 'd') {
-      const iconName = getIconName(component)
-      if (iconName) {
-        ret += ` ${prop}={${iconName}}`
+  if (allowProps) {
+    for (const prop in component.props) {
+      if (component.name === 'Path' && prop === 'd') {
+        const iconName = getIconName(component)
+        if (iconName) {
+          ret += ` ${prop}={${iconName}}`
+        } else {
+          ret += ` ${prop}="${component.props[prop]}"`
+        }
       } else {
         ret += ` ${prop}="${component.props[prop]}"`
       }
-    } else {
-      ret += ` ${prop}="${component.props[prop]}"`
     }
   }
-  ret += '>\n'
-  for (const child of component.children) {
-    ret += calculateComponentNode(child, level + 2)
+  if (selfClosing) {
+    ret += ' />\n'
+    return ret
   }
-  if (component.text) {
-    ret += ' '.repeat(level + 2)
-    ret += `{data['${component.id}']}\n`
+  ret += '>\n'
+  if (allowChildren) {
+    for (const child of component.children) {
+      ret += calculateComponentNode(child, level + 2)
+    }
+    if (component.text) {
+      ret += ' '.repeat(level + 2)
+      ret += `{data['${escapeSingleQuoted(component.id)}']}\n`
+    }
   }
   ret += ' '.repeat(level)
-  ret += `</${component.name.toLowerCase()}>\n`
+  ret += `</${tagName}>\n`
   return ret
 }
 
--- src/lib/export.test.js
+++ src/lib/export.test.js
@@ -49,4 +49,88 @@ describe('export helpers', () => {
     expect(output).toContain('<path d="M1 2 3 4">')
     expect(output).not.toContain('<path d={}>')
   })
+
+  test('escapes single quotes in exported data object', () => {
+    const design = [
+      {
+        id: "hero'quote",
+        name: 'Div',
+        title: '',
+        props: {},
+        css: {},
+        text: "It's fine",
+        children: [],
+      },
+    ]
+
+    const output = renderSvelte(design, {})
+    expect(output).toContain("'hero\\'quote': 'It\\'s fine',")
+    expect(output).toContain("{data['hero\\'quote']}")
+  })
+
+  test('does not export children/text for nochildren elements and removes props for noprops elements', () => {
+    const design = [
+      {
+        id: 'in1',
+        name: 'Input',
+        title: '',
+        props: { type: 'text', value: 'abc' },
+        css: { color: 'red' },
+        text: 'hidden text',
+        children: [
+          {
+            id: 'nested',
+            name: 'Span',
+            title: '',
+            props: {},
+            css: {},
+            text: 'child text',
+            children: [],
+          },
+        ],
+      },
+      {
+        id: 'ta1',
+        name: 'Textarea',
+        title: '',
+        props: { rows: '4' },
+        css: { color: 'blue' },
+        text: 'textarea text',
+        children: [
+          {
+            id: 'nested2',
+            name: 'Span',
+            title: '',
+            props: {},
+            css: {},
+            text: 'child text 2',
+            children: [],
+          },
+        ],
+      },
+      {
+        id: 'br1',
+        name: 'Br',
+        title: '',
+        props: { id: 'break' },
+        css: { color: 'green' },
+        text: '',
+        children: [],
+      },
+    ]
+
+    const output = renderSvelte(design, {})
+    expect(output).toContain('<input class="in1" type="text" value="abc" />')
+    expect(output).not.toContain('hidden text')
+    expect(output).not.toContain('nested')
+    expect(output).toContain('<textarea>')
+    expect(output).toContain('</textarea>')
+    expect(output).not.toContain('rows="4"')
+    expect(output).not.toContain('class="ta1"')
+    expect(output).toContain('<br />')
+    expect(output).not.toContain('id="break"')
+    expect(output).not.toContain('class="br1"')
+    expect(output).not.toContain("'in1':")
+    expect(output).not.toContain("'ta1':")
+  })
 })
